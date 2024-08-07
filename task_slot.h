9db632772d0cf4829bbd39b6a008f5d71f5afe75
Artem(Artyom) <77617130+PlayinPistols2d@users.noreply.github.com>
2024-08-07 09:55:25 +0300

    Create task_slot.h

---
+++ task_slot.h
@@ -0,0 +1,43 @@
+#ifndef TASKSLOT_H
+#define TASKSLOT_H
+
+#include <QWidget>
+#include <QLabel>
+#include <QDragEnterEvent>
+#include <QDropEvent>
+#include <QMimeData>
+#include <QVBoxLayout>
+#include "employee_card.h"
+
+namespace Ui {
+class TaskSlot;
+}
+
+class TaskSlot : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit TaskSlot(QWidget *parent = nullptr);
+    ~TaskSlot();
+
+    void setJobRequirement(const QString &job);
+    QString jobRequirement() const;
+    void lockSlot();
+    void unlockSlot();
+
+protected:
+    void dragEnterEvent(QDragEnterEvent *event) override;
+    void dragLeaveEvent(QDragLeaveEvent *event) override;
+    void dropEvent(QDropEvent *event) override;
+
+private:
+    Ui::TaskSlot *ui;
+    QString m_jobRequirement;
+    bool m_locked;
+    EmployeeCard *m_employeeCard;
+
+    void updateStyle();
+};
+
+#endif // TASKSLOT_H
