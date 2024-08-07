7e300a4c65415629bc128d676bb958b0c78812e7
Artem(Artyom) <77617130+PlayinPistols2d@users.noreply.github.com>
2024-08-07 10:48:59 +0300

    Create side_panel.cpp

---
+++ side_panel.cpp
@@ -0,0 +1,50 @@
+#include "side_panel.h"
+#include "ui_side_panel.h"
+
+SidePanel::SidePanel(QWidget *parent) :
+    QWidget(parent),
+    ui(new Ui::SidePanel)
+{
+    ui->setupUi(this);
+
+    ui->comboBox_filter->addItem("All");
+    ui->comboBox_filter->addItem("Developer");
+    ui->comboBox_filter->addItem("Designer");
+    connect(ui->comboBox_filter, &QComboBox::currentTextChanged, this, &SidePanel::onFilterChanged);
+}
+
+SidePanel::~SidePanel()
+{
+    delete ui;
+}
+
+void SidePanel::addEmployeeCard(EmployeeCard *card)
+{
+    m_employeeCards.append(card);
+    ui->scrollAreaLayout->addWidget(card);
+    card->show();
+}
+
+void SidePanel::removeEmployeeCard(EmployeeCard *card)
+{
+    m_employeeCards.removeOne(card);
+    ui->scrollAreaLayout->removeWidget(card);
+    card->hide();
+}
+
+void SidePanel::onFilterChanged(const QString &filter)
+{
+    updateDisplayedCards();
+}
+
+void SidePanel::updateDisplayedCards()
+{
+    QString filter = ui->comboBox_filter->currentText();
+    for (EmployeeCard *card : qAsConst(m_employeeCards)) {
+        if (filter == "All" || card->jobPosition() == filter) {
+            card->show();
+        } else {
+            card->hide();
+        }
+    }
+}
