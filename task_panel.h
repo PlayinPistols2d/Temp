6d5c18dc1830635e33c7c683fd87644fc9ed3fa1
Artem(Artyom) <77617130+PlayinPistols2d@users.noreply.github.com>
2024-08-07 10:28:19 +0300

    Create task_panel.h

---
+++ task_panel.h
@@ -0,0 +1,36 @@
+#ifndef TASKPANEL_H
+#define TASKPANEL_H
+
+#include <QWidget>
+#include <QMap>
+#include "task.h"
+#include "task_slot.h"
+
+namespace Ui {
+class TaskPanel;
+}
+
+class TaskPanel : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit TaskPanel(QWidget *parent = nullptr);
+    ~TaskPanel();
+
+    void setTasks(const QList<Task> &tasks);
+
+private slots:
+    void onTaskChanged(int index);
+    void onConfirm();
+
+private:
+    Ui::TaskPanel *ui;
+    QList<Task> m_tasks;
+    QMap<QString, TaskSlot*> m_taskSlots;
+    Task *m_currentTask;
+
+    void updateTaskSlots();
+};
+
+#endif // TASKPANEL_H
