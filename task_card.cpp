9756cc4e8b2fb54745c1514631756795a47e56ca
Artem(Artyom) <77617130+PlayinPistols2d@users.noreply.github.com>
2024-08-08 14:46:43 +0300

    Create task_card.cpp

---
+++ task_card.cpp
@@ -0,0 +1,107 @@
+#include "task_card.h"
+#include "ui_task_card.h"
+
+TaskCard::TaskCard(Task *task, QWidget *parent) :
+    QWidget(parent),
+    ui(new Ui::TaskCard),
+    m_task(task)
+{
+    ui->setupUi(this);
+    setAcceptDrops(true);
+    ui->label_taskName->setText(task->name());
+    updateTree();
+}
+
+TaskCard::~TaskCard()
+{
+    delete ui;
+}
+
+void TaskCard::mousePressEvent(QMouseEvent *event)
+{
+    if (event->button() == Qt::LeftButton) {
+        dragStartPosition = event->pos();
+    }
+    QWidget::mousePressEvent(event);
+}
+
+void TaskCard::mouseMoveEvent(QMouseEvent *event)
+{
+    if (!(event->buttons() & Qt::LeftButton))
+        return;
+    if ((event->pos() - dragStartPosition).manhattanLength() < QApplication::startDragDistance())
+        return;
+
+    QDrag *drag = new QDrag(this);
+    QMimeData *mimeData = new QMimeData;
+
+    mimeData->setText("TaskCard");
+    drag->setMimeData(mimeData);
+
+    QPixmap pixmap = this->grab();
+    mimeData->setImageData(pixmap);
+    drag->setPixmap(pixmap);
+    drag->setHotSpot(event->pos()); // Set cursor in the middle of the card
+
+    drag->exec(Qt::MoveAction);
+}
+
+void TaskCard::dragEnterEvent(QDragEnterEvent *event)
+{
+    if (event->mimeData()->hasText() && event->mimeData()->text() == "EmployeeCard") {
+        event->acceptProposedAction();
+    } else {
+        event->ignore();
+    }
+}
+
+void TaskCard::dragMoveEvent(QDragMoveEvent *event)
+{
+    if (event->mimeData()->hasText() && event->mimeData()->text() == "EmployeeCard") {
+        event->acceptProposedAction();
+    } else {
+        event->ignore();
+    }
+}
+
+void TaskCard::dropEvent(QDropEvent *event)
+{
+    if (event->mimeData()->hasText() && event->mimeData()->text() == "EmployeeCard") {
+        EmployeeCard *employeeCard = qobject_cast<EmployeeCard*>(event->source());
+        if (employeeCard) {
+            // Add employee to the task
+            m_task->assignEmployee(employeeCard->jobPosition(), employeeCard);
+            updateTree();
+            event->acceptProposedAction();
+        } else {
+            event->ignore();
+        }
+    } else {
+        event->ignore();
+    }
+}
+
+void TaskCard::populateTree(Task *task, QTreeWidgetItem *parentItem)
+{
+    QTreeWidgetItem *taskItem = new QTreeWidgetItem(parentItem);
+    taskItem->setText(0, task->name());
+    parentItem->addChild(taskItem);
+
+    for (const QString &job : task->requirements().keys()) {
+        QTreeWidgetItem *jobItem = new QTreeWidgetItem(taskItem);
+        jobItem->setText(0, job + " (Hard: " + QString::number(task->requirements()[job].first) + 
+                         ", Soft: " + QString::number(task->assignedEmployees()[job].size() - task->requirements()[job].first) + ")");
+        taskItem->addChild(jobItem);
+    }
+
+    for (Task *childTask : task->children()) {
+        populateTree(childTask, taskItem);
+    }
+}
+
+void TaskCard::updateTree()
+{
+    ui->treeWidget_tasks->clear();
+    populateTree(m_task, ui->treeWidget_tasks->invisibleRootItem());
+    ui->treeWidget_tasks->expandAll();
+}
