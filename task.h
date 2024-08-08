c6aafe57d8606a575f81c6bd38641d7e2938fb2e
Artem(Artyom) <77617130+PlayinPistols2d@users.noreply.github.com>
2024-08-08 14:48:10 +0300

    Update task.h

--- task.h
+++ task.h
@@ -16,9 +16,11 @@ public:
     
     QString name() const;
     void addRequirement(const QString &job, int count, RequirementType type);
+    void addChild(Task *child);
 
     QMap<QString, QPair<int, RequirementType>> requirements() const;
     QMap<QString, QList<EmployeeCard*>> assignedEmployees() const;
+    QList<Task*> children() const;
 
     void assignEmployee(const QString &job, EmployeeCard *employee);
     void unassignEmployee(const QString &job, EmployeeCard *employee);
@@ -28,6 +30,7 @@ private:
     QString m_name;
     QMap<QString, QPair<int, RequirementType>> m_requirements; // job -> (count, type)
     QMap<QString, QList<EmployeeCard*>> m_assignedEmployees; // job -> list of EmployeeCard*
+    QList<Task*> m_children; // List of child tasks
 };
 
 #endif // TASK_H
