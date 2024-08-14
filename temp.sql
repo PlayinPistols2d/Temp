d90238d9b35a30615a99b2ad6bd10750f6b143fe
Artem(Artyom) <77617130+PlayinPistols2d@users.noreply.github.com>
2024-08-14 12:19:51 +0300

    Update temp.sql

--- temp.sql
+++ temp.sql
@@ -7,7 +7,8 @@ task_hierarchy AS (
     SELECT 
         o.id,
         o.name,
-        o.type,
+        t.name AS type_name,
+        g.name AS group_name,
         o.parent_operation_id,
         o.priority,
         o.post_id,
@@ -16,6 +17,15 @@ task_hierarchy AS (
     FROM operations o
     INNER JOIN posts p ON o.post_id = p.id
     INNER JOIN params ON p.post_number = params.post_number
+    
+    -- Joining to the many-to-many table and then to the types table to get the exact type name
+    LEFT JOIN opt ON o.id = opt.operation_id
+    LEFT JOIN types t ON opt.type_id = t.id
+
+    -- Joining to the many-to-many table and then to the groups table to get the actual group name
+    LEFT JOIN operation_user_groups oug ON o.id = oug.operation_id
+    LEFT JOIN groups g ON oug.group_id = g.id
+
     WHERE o.parent_operation_id IS NULL
 
     UNION ALL
@@ -24,7 +34,8 @@ task_hierarchy AS (
     SELECT 
         o.id,
         o.name,
-        o.type,
+        t.name AS type_name,
+        g.name AS group_name,
         o.parent_operation_id,
         o.priority,
         o.post_id,
@@ -32,6 +43,15 @@ task_hierarchy AS (
         th.depth + 1 AS depth
     FROM operations o
     INNER JOIN task_hierarchy th ON o.parent_operation_id = th.id
+    
+    -- Joining to the many-to-many table and then to the types table to get the exact type name
+    LEFT JOIN opt ON o.id = opt.operation_id
+    LEFT JOIN types t ON opt.type_id = t.id
+
+    -- Joining to the many-to-many table and then to the groups table to get the actual group name
+    LEFT JOIN operation_user_groups oug ON o.id = oug.operation_id
+    LEFT JOIN groups g ON oug.group_id = g.id
+
     WHERE o.post_id = th.post_id
 )
 
@@ -39,7 +59,8 @@ task_hierarchy AS (
 SELECT 
     id, 
     name, 
-    type, 
+    type_name, 
+    group_name, 
     parent_operation_id, 
     priority, 
     post_id
