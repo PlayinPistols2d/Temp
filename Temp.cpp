0b29e80f1c105a65dfaa3ed03f6cf67e004ab8ac
Artem(Artyom) <77617130+PlayinPistols2d@users.noreply.github.com>
2024-08-14 15:00:12 +0300

    Update Temp.cpp

--- Temp.cpp
+++ Temp.cpp
@@ -1,9 +1,10 @@
-QList<Task*> parseTasksFromDatabase(const QList<QList<QVariant>>& rows)
+QList<TaskCard*> parseTasksToTaskCards(const QList<QList<QVariant>>& rows, QWidget *parent = nullptr)
 {
-    QList<Task*> taskList;
+    QList<TaskCard*> taskCards;
     QMap<int, Task*> taskMap; // To keep track of tasks by their ID
 
     Task* lastTask = nullptr;
+    Task* currentParentTask = nullptr;
 
     for (const QList<QVariant>& row : rows) {
         // Assuming row contains: Id, Name, Type, Group, Parent_Id, Priority
@@ -21,8 +22,15 @@ QList<Task*> parseTasksFromDatabase(const QList<QList<QVariant>>& rows)
         taskMap[id] = task;
 
         if (parentId == 0 || parentId == -1) {
-            // This is a root-level task, add it to the main list
-            taskList.append(task);
+            // This is a root-level task (a new parent task)
+            if (currentParentTask != nullptr) {
+                // Create a TaskCard for the previous parent task
+                TaskCard* taskCard = new TaskCard(currentParentTask, parent);
+                taskCards.append(taskCard);
+            }
+
+            // Set this as the new parent task
+            currentParentTask = task;
         } else {
             // This task has a parent, find the parent task
             Task* parentTask = taskMap.value(parentId, nullptr);
@@ -37,5 +45,11 @@ QList<Task*> parseTasksFromDatabase(const QList<QList<QVariant>>& rows)
         lastTask = task;
     }
 
-    return taskList;
+    // After the loop, create a TaskCard for the last parent task
+    if (currentParentTask != nullptr) {
+        TaskCard* taskCard = new TaskCard(currentParentTask, parent);
+        taskCards.append(taskCard);
+    }
+
+    return taskCards;
 }
