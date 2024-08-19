6e73d95b8a00f05bf9a2dece451859415ea2706e
Artem(Artyom) <77617130+PlayinPistols2d@users.noreply.github.com>
2024-08-19 11:58:10 +0300

    Update loading_screen.cpp

--- loading_screen.cpp
+++ loading_screen.cpp
@@ -2,27 +2,32 @@
 
 LoadingScreen::LoadingScreen(QWidget *parent) : QWidget(parent)
 {
-    // Make sure the loading screen covers the entire parent window
+    // Set the widget to be transparent and frameless
+    setAttribute(Qt::WA_TransparentForMouseEvents); // Allows clicks to pass through the loading screen
+    setAttribute(Qt::WA_NoSystemBackground);
+    setAttribute(Qt::WA_OpaquePaintEvent, false);
+    setWindowFlags(Qt::FramelessWindowHint | Qt::WindowStaysOnTopHint | Qt::SubWindow);
+
+    // Make sure the loading screen covers the entire parent widget
     setGeometry(parent->rect());
 
     // Set the blur effect
     backgroundLabel = new QLabel(this);
-    backgroundLabel->setGeometry(this->rect());
     backgroundLabel->setStyleSheet("background-color: rgba(0, 0, 0, 100);"); // Semi-transparent background
 
-    QGraphicsBlurEffect *blurEffect = new QGraphicsBlurEffect(this);
+    blurEffect = new QGraphicsBlurEffect(this);
     blurEffect->setBlurRadius(10);
     backgroundLabel->setGraphicsEffect(blurEffect);
 
     // Set up the loading SVG
     loadingSvg = new QSvgWidget(":/path/to/your/loading.svg", this);
     loadingSvg->setFixedSize(100, 100); // Set desired size
-    loadingSvg->move((width() - loadingSvg->width()) / 2, (height() - loadingSvg->height()) / 2);
 
     // Set up the layout (optional)
     QVBoxLayout *layout = new QVBoxLayout(this);
     layout->addWidget(backgroundLabel);
-    layout->addWidget(loadingSvg, 0, Qt::AlignCenter);
+    layout->setContentsMargins(0, 0, 0, 0); // No margins, fill the entire widget
+    layout->setSpacing(0);
 
     // Initially hidden
     setVisible(false);
@@ -30,13 +35,23 @@ LoadingScreen::LoadingScreen(QWidget *parent) : QWidget(parent)
 
 void LoadingScreen::start()
 {
+    // Move the loading spinner to the center of the parent
+    loadingSvg->move((width() - loadingSvg->width()) / 2, (height() - loadingSvg->height()) / 2);
     setVisible(true);
-    loadingSvg->show();
 }
 
 void LoadingScreen::stop()
 {
-    loadingSvg->hide();
     setVisible(false);
     emit loadingFinished();
+}
+
+void LoadingScreen::resizeEvent(QResizeEvent *event)
+{
+    QWidget::resizeEvent(event);
+
+    // Resize and reposition the background and loading indicator to match the parent widget
+    setGeometry(parentWidget()->rect());
+    backgroundLabel->setGeometry(this->rect());
+    loadingSvg->move((width() - loadingSvg->width()) / 2, (height() - loadingSvg->height()) / 2);
 }
