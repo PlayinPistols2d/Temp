374aaf4d3f0bdb3cc843494db400cfab99aa13d0
Artem(Artyom) <77617130+PlayinPistols2d@users.noreply.github.com>
2024-08-19 11:38:28 +0300

    Update LoadingScreen.h

--- LoadingScreen.h
+++ LoadingScreen.h
@@ -2,39 +2,26 @@
 #define LOADINGSCREEN_H
 
 #include <QWidget>
-#include <QSvgRenderer>
+#include <QGraphicsBlurEffect>
 #include <QLabel>
 #include <QVBoxLayout>
-#include <QGraphicsBlurEffect>
-#include <QTimer>
+#include <QSvgWidget>
 
-class LoadingScreen : public QWidget {
+class LoadingScreen : public QWidget
+{
     Q_OBJECT
-
 public:
     explicit LoadingScreen(QWidget *parent = nullptr);
+
     void start();
     void stop();
-    void setLoadingSvg(const QString &svgPath);
-    void setBlurLevel(int level);
-    void bindToProcess(QObject *process, const char *startSignal, const char *stopSignal);
-
-private:
-    QLabel *backgroundLabel;
-    QLabel *loadingLabel;
-    QSvgRenderer *svgRenderer;
-    QTimer *updateTimer;
-    QGraphicsBlurEffect *blurEffect;
-    QVBoxLayout *layout;
-
-    void setupUI();
-    void updateSvg();
 
 signals:
     void loadingFinished();
 
-public slots:
-    void onProcessFinished();
+private:
+    QLabel *backgroundLabel;
+    QSvgWidget *loadingSvg;
 };
 
 #endif // LOADINGSCREEN_H
