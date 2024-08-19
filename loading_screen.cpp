bcf61dfcc421bcb19f384cdd9f1604daba93848f
Artem(Artyom) <77617130+PlayinPistols2d@users.noreply.github.com>
2024-08-19 11:38:50 +0300

    Update loading_screen.cpp

--- loading_screen.cpp
+++ loading_screen.cpp
@@ -1,73 +1,42 @@
 #include "LoadingScreen.h"
-#include <QPainter>
 
-LoadingScreen::LoadingScreen(QWidget *parent) : QWidget(parent) {
-    setWindowFlags(Qt::FramelessWindowHint | Qt::WindowStaysOnTopHint | Qt::CustomizeWindowHint);
-    setAttribute(Qt::WA_TranslucentBackground);
-    setAttribute(Qt::WA_ShowWithoutActivating);
+LoadingScreen::LoadingScreen(QWidget *parent) : QWidget(parent)
+{
+    // Make sure the loading screen covers the entire parent window
+    setGeometry(parent->rect());
 
-    setupUI();
-}
-
-void LoadingScreen::setupUI() {
+    // Set the blur effect
     backgroundLabel = new QLabel(this);
-    backgroundLabel->setStyleSheet("background-color: rgba(0, 0, 0, 150);"); // Semi-transparent black
+    backgroundLabel->setGeometry(this->rect());
+    backgroundLabel->setStyleSheet("background-color: rgba(0, 0, 0, 100);"); // Semi-transparent background
 
-    loadingLabel = new QLabel(this);
-    loadingLabel->setAlignment(Qt::AlignCenter);
-
-    svgRenderer = new QSvgRenderer(this);
-
-    blurEffect = new QGraphicsBlurEffect(this);
+    QGraphicsBlurEffect *blurEffect = new QGraphicsBlurEffect(this);
     blurEffect->setBlurRadius(10);
     backgroundLabel->setGraphicsEffect(blurEffect);
 
-    layout = new QVBoxLayout(this);
-    layout->addWidget(loadingLabel, 0, Qt::AlignCenter);
+    // Set up the loading SVG
+    loadingSvg = new QSvgWidget(":/path/to/your/loading.svg", this);
+    loadingSvg->setFixedSize(100, 100); // Set desired size
+    loadingSvg->move((width() - loadingSvg->width()) / 2, (height() - loadingSvg->height()) / 2);
 
-    setLayout(layout);
-    setFixedSize(parentWidget()->size());
+    // Set up the layout (optional)
+    QVBoxLayout *layout = new QVBoxLayout(this);
+    layout->addWidget(backgroundLabel);
+    layout->addWidget(loadingSvg, 0, Qt::AlignCenter);
 
-    // Timer to update the SVG animation (if needed)
-    updateTimer = new QTimer(this);
-    connect(updateTimer, &QTimer::timeout, this, &LoadingScreen::updateSvg);
+    // Initially hidden
+    setVisible(false);
 }
 
-void LoadingScreen::start() {
-    updateTimer->start(16); // Approximately 60 FPS for smooth animation
-    show();
+void LoadingScreen::start()
+{
+    setVisible(true);
+    loadingSvg->show();
 }
 
-void LoadingScreen::stop() {
-    updateTimer->stop();
-    hide();
+void LoadingScreen::stop()
+{
+    loadingSvg->hide();
+    setVisible(false);
     emit loadingFinished();
-}
-
-void LoadingScreen::setLoadingSvg(const QString &svgPath) {
-    svgRenderer->load(svgPath);
-}
-
-void LoadingScreen::setBlurLevel(int level) {
-    blurEffect->setBlurRadius(level);
-}
-
-void LoadingScreen::bindToProcess(QObject *process, const char *startSignal, const char *stopSignal) {
-    connect(process, startSignal, this, SLOT(start()));
-    connect(process, stopSignal, this, SLOT(stop()));
-    connect(process, stopSignal, this, SLOT(onProcessFinished()));
-}
-
-void LoadingScreen::onProcessFinished() {
-    stop();
-    emit loadingFinished();
-}
-
-void LoadingScreen::updateSvg() {
-    QPixmap pixmap(loadingLabel->size());
-    pixmap.fill(Qt::transparent);
-
-    QPainter painter(&pixmap);
-    svgRenderer->render(&painter);
-    loadingLabel->setPixmap(pixmap);
 }
