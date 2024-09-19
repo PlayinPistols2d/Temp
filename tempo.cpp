e887e54e3ead89ef73209fe9f5212532ed6a9af1
Artem(Artyom) <77617130+PlayinPistols2d@users.noreply.github.com>
2024-09-19 09:58:12 +0300

    Update tempo.cpp

--- tempo.cpp
+++ tempo.cpp
@@ -1,29 +1,13 @@
-#include <QObject>
-#include <QMetaMethod>
-#include <QDebug>
-
-void listConnections(QObject* sender, const char* signal)
-{
-    const QMetaObject* metaObject = sender->metaObject();
-    int signalIndex = metaObject->indexOfSignal(QMetaObject::normalizedSignature(signal).constData());
-    
-    if (signalIndex < 0) {
-        qWarning() << "Signal not found!";
-        return;
+void OperationManager::deleteOperations() {
+    for (Operation* op : rootOperations) {
+        deleteOperation(op);
     }
+    rootOperations.clear();
+}
 
-    QMetaMethod signalMethod = metaObject->method(signalIndex);
-    
-    // Introspect the sender object for connections
-    const auto* connections = QObjectPrivate::get(sender)->connections();
-    for (auto it = connections->begin(); it != connections->end(); ++it) {
-        if (it->signalIndex == signalIndex) {
-            QObject* receiver = it->receiver;
-            int slotIndex = receiver->metaObject()->indexOfMethod(it->method);
-            QMetaMethod slotMethod = receiver->metaObject()->method(slotIndex);
-
-            qDebug() << "Signal" << signalMethod.methodSignature() << "is connected to"
-                     << "Slot" << slotMethod.methodSignature() << "of object" << receiver;
-        }
+void OperationManager::deleteOperation(Operation* op) {
+    for (Operation* child : op->children) {
+        deleteOperation(child);
     }
+    delete op;
 }
