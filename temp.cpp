19aa0ea0fc14ef13edc595c888de08df8b51e0c8
Artem(Artyom) <77617130+PlayinPistols2d@users.noreply.github.com>
2024-10-01 10:14:43 +0300

    Update temp.cpp

--- temp.cpp
+++ temp.cpp
@@ -1,75 +1,69 @@
-#ifndef BARCODESCANNER_H
-#define BARCODESCANNER_H
-
-#include <QObject>
-#include <QUdpSocket>
-#include <QString>
-
-class BarcodeScanner : public QObject {
-    Q_OBJECT
-
-public:
-    BarcodeScanner(const QString &serverAddress, quint16 serverPort);
-    void startReading();  // Starts reading from the input
-
-private:
-    QUdpSocket *m_udpSocket;
-    QString m_serverAddress;
-    quint16 m_serverPort;
-
-    void sendBarcode(const QString &barcode);  // Sends the barcode to the server
-};
-
-#endif // BARCODESCANNER_H
-
-
-
-
-#include "barcode_scanner.h"
+#include "barcodelistener.h"
 #include <QTextStream>
-#include <QHostAddress>
 #include <QDebug>
 
-BarcodeScanner::BarcodeScanner(const QString &serverAddress, quint16 serverPort)
-    : m_serverAddress(serverAddress), m_serverPort(serverPort) {
-    m_udpSocket = new QUdpSocket(this);
+BarcodeListener::BarcodeListener(QObject *parent)
+    : QObject(parent), udpSocket(new QUdpSocket(this)), inputNotifier(new QSocketNotifier(fileno(stdin), QSocketNotifier::Read, this))
+{
+    connect(inputNotifier, &QSocketNotifier::activated, this, &BarcodeListener::onBarcodeReceived);
 }
 
-void BarcodeScanner::startReading() {
+void BarcodeListener::onBarcodeReceived()
+{
     QTextStream input(stdin);
-    QString barcode;
-    qDebug() << "Waiting for barcode input...";
-
-    while (true) {
-        barcode = input.readLine();
-        if (!barcode.isEmpty()) {
-            sendBarcode(barcode);
-        }
+    QString barcode = input.readLine().trimmed();
+    
+    if (!barcode.isEmpty()) {
+        qDebug() << "Barcode received:" << barcode;
+        sendBarcodeToServer(barcode);
     }
 }
 
-void BarcodeScanner::sendBarcode(const QString &barcode) {
-    QByteArray data = barcode.toUtf8();
-    m_udpSocket->writeDatagram(data, QHostAddress(m_serverAddress), m_serverPort);
-    qDebug() << "Barcode sent: " << barcode;
+void BarcodeListener::sendBarcodeToServer(const QString &barcode)
+{
+    QByteArray datagram = barcode.toUtf8();
+    QHostAddress serverAddress("127.0.0.1");  // Replace with your server's IP address
+    quint16 serverPort = 12345;               // Replace with your server's UDP port
+    
+    udpSocket->writeDatagram(datagram, serverAddress, serverPort);
+    qDebug() << "Barcode sent to server.";
 }
 
 
+#ifndef BARCODERLISTENER_H
+#define BARCODERLISTENER_H
+
+#include <QObject>
+#include <QUdpSocket>
+#include <QSocketNotifier>
+
+class BarcodeListener : public QObject {
+    Q_OBJECT
+
+public:
+    explicit BarcodeListener(QObject *parent = nullptr);
+
+private slots:
+    void onBarcodeReceived();
+    void sendBarcodeToServer(const QString &barcode);
+
+private:
+    QUdpSocket *udpSocket;
+    QSocketNotifier *inputNotifier;
+};
+
+#endif // BARCODERLISTENER_H
 
 
 #include <QCoreApplication>
-#include "barcode_scanner.h"
+#include "barcodelistener.h"
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
     QCoreApplication a(argc, argv);
 
-    QString serverAddress = "127.0.0.1";  // Set your server IP
-    quint16 serverPort = 12345;           // Set your server port
-
-    BarcodeScanner scanner(serverAddress, serverPort);
-    
-    // Start reading input from the barcode scanner
-    scanner.startReading();
+    BarcodeListener listener;
 
     return a.exec();
 }
+
