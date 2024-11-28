30e4ec6b8b2ebad3983c2bdefaeacccce10aa894
Artem(Artyom) <77617130+PlayinPistols2d@users.noreply.github.com>
2024-11-28 15:45:13 +0300

    Update temp.sql

--- temp.sql
+++ temp.sql
@@ -1,28 +1,18 @@
-BEGIN;
-
--- Создаем временную таблицу для хранения ID
-CREATE TEMP TABLE temp_ids (id INTEGER);
-
--- Добавляем первые 10 вставок и сохраняем ID
-INSERT INTO your_table (column1, column2)
-VALUES ('value1', 'value2')
-RETURNING id INTO temp_ids;
-
-INSERT INTO your_table (column1, column2)
-VALUES ('value3', 'value4')
-RETURNING id INTO temp_ids;
-
--- Дополнительные запросы, использующие сохраненные ID
-INSERT INTO another_table (foreign_key, column)
-SELECT id, 'valueX'
-FROM temp_ids;
-
--- Другие действия, которые требуют ID
-UPDATE another_table
-SET some_column = 'updated_value'
-WHERE foreign_key IN (SELECT id FROM temp_ids);
-
--- Очистка временной таблицы (если не требуется более)
-DROP TABLE temp_ids;
-
-COMMIT;
+WITH first_insert AS (
+    INSERT INTO your_table (column1, column2)
+    VALUES ('value1', 'value2')
+    RETURNING id AS first_id
+),
+second_insert AS (
+    INSERT INTO your_table (column1, column2, parent_id)
+    VALUES ('value3', 'value4', (SELECT first_id FROM first_insert))
+    RETURNING id AS second_id
+),
+third_insert AS (
+    INSERT INTO your_table (column1, column2, parent_id)
+    VALUES ('value5', 'value6', (SELECT second_id FROM second_insert))
+    RETURNING id AS third_id
+)
+INSERT INTO final_table (foreign_key, column)
+SELECT third_id, 'final_value'
+FROM third_insert;
