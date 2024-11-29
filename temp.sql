a1c2373662c14a790709ebac760f7c1ac6dd742a
Artem(Artyom) <77617130+PlayinPistols2d@users.noreply.github.com>
2024-11-29 10:54:48 +0300

    Update temp.sql

--- temp.sql
+++ temp.sql
@@ -1,14 +1,24 @@
-BEGIN;
+CREATE OR REPLACE FUNCTION insert_and_process(
+    static_arg1 TEXT,
+    static_arg2 TEXT,
+    static_arg3 TEXT,
+    static_arg4 TEXT,
+    input_data TEXT[],
+    insert_count INT
+) RETURNS VOID AS $$
+DECLARE
+    new_id INT;
+    i INT;
+BEGIN
+    -- Insert static data into the first table
+    INSERT INTO static_table (column1, column2, column3, column4)
+    VALUES (static_arg1, static_arg2, static_arg3, static_arg4)
+    RETURNING id INTO new_id;
 
--- Шаг 1: Инсерт в первую таблицу
-INSERT INTO first_table (column1, column2)
-VALUES ('value1', 'value2');
-
--- Шаг 2: Получение последнего вставленного ID
-SELECT LASTVAL() INTO last_inserted_id;
-
--- Шаг 3: Использование этого ID для инсерта во вторую таблицу
-INSERT INTO second_table (first_table_id, other_column)
-VALUES (last_inserted_id, 'value_for_second_table');
-
-COMMIT;
+    -- Perform multiple inserts using the returned id
+    FOR i IN 1..insert_count LOOP
+        INSERT INTO related_table (static_id, data)
+        VALUES (new_id, input_data[i]);
+    END LOOP;
+END;
+$$ LANGUAGE plpgsql;
