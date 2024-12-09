2b64c46c5a7e7976a7671e3361b548e34df00870
Artem(Artyom) <77617130+PlayinPistols2d@users.noreply.github.com>
2024-12-09 12:55:36 +0300

    Update temp.cpp

--- temp.cpp
+++ temp.cpp
@@ -1,27 +1,34 @@
-// main.cpp or some usage point
-
-#include "CustomSelector.h"
 #include <QApplication>
+#include <QWidget>
+#include <QVBoxLayout>
+#include <QDebug>
+#include "CustomLineEdit.h"
 
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
 
-    CustomSelector selector;
+    QWidget w;
+    QVBoxLayout layout(&w);
+
+    CustomLineEdit *lineEdit = new CustomLineEdit;
+    layout.addWidget(lineEdit);
+
     QList<QPair<QString, QString>> items = {
         { "Apple", "A sweet red fruit." },
         { "Apricot", "A small, yellow-orange stone fruit." },
         { "Avocado", "A creamy green fruit used in guacamole." },
-        { "Banana", "A long curved fruit with a yellow skin." }
+        { "Banana", "A long curved fruit with a yellow skin." },
+        { "Blueberry", "A small sweet blue fruit." }
     };
-    selector.setItems(items);
+    lineEdit->setItems(items);
 
-    QObject::connect(&selector, &CustomSelector::itemSelected, [](const QString &text, const QString &hint){
+    QObject::connect(lineEdit, &CustomLineEdit::itemSelected, [](const QString &text, const QString &hint){
         qDebug() << "Selected:" << text << "Hint:" << hint;
     });
 
-    selector.resize(200, 40);
-    selector.show();
+    w.resize(300, 100);
+    w.show();
 
     return app.exec();
 }
