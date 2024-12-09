771dc8ecc5cf1031b169d50490ffbd17b229578b
Artem(Artyom) <77617130+PlayinPistols2d@users.noreply.github.com>
2024-12-09 12:54:59 +0300

    Update customselector.h

--- customselector.h
+++ customselector.h
@@ -1,54 +1,38 @@
-// CustomSelector.h
 #pragma once
 
-#include <QWidget>
 #include <QLineEdit>
-#include <QListView>
+#include <QCompleter>
 #include <QStandardItemModel>
-#include <QSortFilterProxyModel>
-#include <QVBoxLayout>
-#include <QFrame>
-#include <QEvent>
-#include <QHelpEvent>
 #include <QToolTip>
 
-class CustomSelector : public QWidget
+class CustomLineEdit : public QLineEdit
 {
     Q_OBJECT
 public:
-    explicit CustomSelector(QWidget *parent = nullptr);
+    explicit CustomLineEdit(QWidget *parent = nullptr);
 
     void addItem(const QString &text, const QString &hint);
     void setItems(const QList<QPair<QString, QString>> &items);
     void clearItems();
-    
-    void setFilterCaseSensitivity(Qt::CaseSensitivity caseSensitivity);
-
-    QString currentText() const;
-    QString currentHint() const;
+    void setFilterCaseSensitivity(Qt::CaseSensitivity cs);
 
 signals:
     void itemSelected(const QString &text, const QString &hint);
 
 protected:
-    bool eventFilter(QObject* obj, QEvent* event) override;
+    void focusInEvent(QFocusEvent *e) override;
+    void mousePressEvent(QMouseEvent *e) override;
+    bool eventFilter(QObject *obj, QEvent *event) override;
 
 private slots:
-    void onTextChanged(const QString &text);
-    void onItemClicked(const QModelIndex &index);
-    void onCurrentChanged(const QModelIndex &current, const QModelIndex &previous);
+    void onCompletionSelected(const QString &completion);
+    void onHighlighted(const QString &highlightedText);
 
 private:
-    QLineEdit *m_lineEdit;
-    QFrame *m_popupFrame;
-    QListView *m_listView;
-    QStandardItemModel *m_model;
-    QSortFilterProxyModel *m_proxyModel;
-
-    // To store hints separately for quick lookup; alternatively, store them directly in item data
-    // e.g., item->setData(hint, Qt::UserRole)
+    void showCompleterPopup();
     QString hintForIndex(const QModelIndex &index) const;
-    void showPopup();
-    void hidePopup();
-    void updateHintDisplay(const QModelIndex &index);
+    void showHintForIndex(const QModelIndex &index);
+
+    QCompleter *m_completer;
+    QStandardItemModel *m_model;
 };
